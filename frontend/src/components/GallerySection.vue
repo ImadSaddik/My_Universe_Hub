6f42fa62751b1a36023bb084b1cdc30526ab1851
Imad Saddik <simad3647@gmail.com>
2023-08-27 08:32:41 +0100

    limit number of images in the home page

--- frontend/src/components/GallerySection.vue
+++ frontend/src/components/GallerySection.vue
@@ -2,23 +2,28 @@
   <div class="container-fluid mt-3">
     <div class="row gx-3">
       <GalleryColumn
-        :archive="archive"
+        :archive="limitedArchive"
         :sliceStart="0"
-        :sliceEnd="Math.floor(archive.length / 3)"
+        :sliceEnd="Math.ceil(limitedArchive.length / 3)"
         @selected-item="$emit('selected-item', $event)"
       />
       <GalleryColumn
-        :archive="archive"
-        :sliceStart="Math.floor(archive.length / 3)"
-        :sliceEnd="Math.floor(2 * archive.length / 3)"
+        :archive="limitedArchive"
+        :sliceStart="Math.ceil(limitedArchive.length / 3)"
+        :sliceEnd="Math.ceil(2 * limitedArchive.length / 3)"
         @selected-item="$emit('selected-item', $event)"
       />
       <GalleryColumn
-        :archive="archive"
-        :sliceStart="Math.floor(2 * archive.length / 3)"
-        :sliceEnd="archive.length"
+        :archive="limitedArchive"
+        :sliceStart="Math.ceil(2 * limitedArchive.length / 3)"
+        :sliceEnd="limitedArchive.length"
         @selected-item="$emit('selected-item', $event)"
       />
+    </div>
+    <div v-if="showLoadMore" class="row mx-0 mb-3">
+      <div type="button" class="col border py-4 d-flex align-items-center justify-content-center" @click="increaseLimit">
+        Load more
+      </div>
     </div>
   </div>
 </template>
@@ -34,10 +39,25 @@ export default {
   },
   data () {
     return {
-      selectedItem: null
+      selectedItem: null,
+      limit: 100,
+      showLoadMore: true
+    }
+  },
+  computed: {
+    limitedArchive () {
+      return this.archive.slice(0, this.limit)
     }
   },
   methods: {
+    increaseLimit () {
+      this.limit += 100
+
+      if (this.limit > this.archive.length) {
+        this.limit = this.archive.length
+        this.showLoadMore = false
+      }
+    }
   }
 }
 </script>
