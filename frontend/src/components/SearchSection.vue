d487bc959cf4b8afc51dc5358979c3948fa0b128
Imad Saddik <simad3647@gmail.com>
2023-08-27 09:06:42 +0100

    made search field rounded

--- frontend/src/components/SearchSection.vue
+++ frontend/src/components/SearchSection.vue
@@ -9,8 +9,14 @@
             <h1 class="display-5 fs-1 fw-bold text-white">APOD</h1>
             <p class="fw-light text-white">Enter a date or keyword to search for an Astronomy Picture of the Day</p>
             <div class="input-group mb-3">
-              <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2">
-              <button class="btn btn-primary" type="button" id="button-addon2">Search</button>
+              <input
+                type="text"
+                class="form-control rounded-pill py-2 px-4"
+                placeholder="Andromeda galaxy"
+                v-model="searchQuery"
+                @keydown.enter.prevent="search"
+                aria-describedby="basic-addon1"
+              >
             </div>
           </div>
         </div>
@@ -27,6 +33,11 @@ export default {
   data () {
     return {
       searchQuery: ''
+    }
+  },
+  methods: {
+    search () {
+      console.log(this.searchQuery)
     }
   }
 }
