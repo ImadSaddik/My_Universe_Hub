f80227132b0273393bd05dccc8ff2731e88267b7
Imad Saddik <simad3647@gmail.com>
2023-08-31 08:36:30 +0100

    login page redesigned

--- frontend/src/views/LogInView.vue
+++ frontend/src/views/LogInView.vue
@@ -1,27 +1,44 @@
 <template>
-  <section class="container-fluid">
-    <div class="row">
-      <div class="col pt-5 d-flex justify-content-center">
-        <img :src="currentImage" class="img-fluid rounded-3" alt="" style="width: 70%;">
-      </div>
-      <div class="col p-5">
-        <h5 class="fs-3 mb-4">Log In</h5>
-        <form class="pe-5" @submit.prevent="submitForm">
-          <div class="mb-3">
-            <label for="exampleInputUsername" class="form-label">Username</label>
-            <input type="text" class="form-control" id="exampleInputUsername" v-model="username">
+  <section class="container-fluid" style="height: calc(100vh - 70px)">
+    <div class="position-relative h-100 d-flex flex-column">
+      <img src="../assets/search_background.jpg" class="img-fluid custom-image" alt="...">
+
+      <div class="container position-absolute top-50 start-50 translate-middle align-self-center">
+        <div class="row px-5 pb-5 d-flex justify-content-center">
+          <div class="col-8">
+            <h1 class="display-5 fs-1 fw-bold text-white mb-4">Log In</h1>
+            <form @submit.prevent="submitForm">
+              <div class="mb-3">
+                <label for="exampleInputUsername" class="form-label text-white">Username</label>
+                <input
+                  type="text"
+                  class="form-control rounded-3 py-2 px-3"
+                  placeholder="Your username"
+                  id="exampleInputUsername"
+                  v-model="username">
+              </div>
+              <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label text-white">Password</label>
+                <input
+                  class="form-control rounded-3 py-2 px-3"
+                  placeholder="Your password"
+                  id="exampleInputPassword1"
+                  v-model="password"
+                  :type="showHidePassword ? 'password' : 'text'">
+              </div>
+              <div class="mb-3 form-check">
+                <label class="form-check-label text-white" for="showHidePasswordCheckBox">{{ showHidePassword ? 'Show password' : 'Hide password' }}</label>
+                <input
+                  type="checkbox"
+                  class="form-check-input"
+                  id="showHidePasswordCheckBox"
+                  @click="showHidePassword = !showHidePassword">
+              </div>
+              <p class="text-white">Or <a href="/signup">Click here</a>, if you don't have an account.</p>
+              <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
           </div>
-          <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">Password</label>
-            <input class="form-control" id="exampleInputPassword1" v-model="password" :type="showHidePassword ? 'password' : 'text'">
-          </div>
-          <div class="mb-3 form-check">
-            <input type="checkbox" class="form-check-input" id="showHidePasswordCheckBox" @click="showHidePassword = !showHidePassword">
-            <label class="form-check-label" for="showHidePasswordCheckBox">{{ showHidePassword ? 'Show password' : 'Hide password' }}</label>
-          </div>
-          <p>Or <a href="/signup">Click here</a>, if you don't have an account.</p>
-          <button type="submit" class="btn btn-primary">Submit</button>
-        </form>
+        </div>
       </div>
     </div>
   </section>
@@ -79,8 +96,7 @@ export default {
           localStorage.setItem('selectedNavbarItem', 'home')
           this.$store.commit('setSelectedNavbarItem', 'home')
 
-          const toPath = this.$route.query.to || '/'
-          this.$router.push(toPath)
+          this.$router.push({ name: 'home', replace: true })
           console.log('successfully logged in')
         })
         .catch(error => {
@@ -93,3 +109,11 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+  .custom-image {
+    height: 100%;
+    object-fit: cover;
+    filter: brightness(0.4);
+  }
+</style>
