f6332bb0a3b391699d9f1ec00c658be9c41d9a58
Imad Saddik <simad3647@gmail.com>
2025-05-28 12:29:45 +0100

    Used eslint-config-prettier to resolve ESLint and Prettier formatting conflicts

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,9 +1,11 @@
 import pluginVue from "eslint-plugin-vue";
 import globals from "globals";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 export default [
   ...pluginVue.configs["flat/recommended"],
   ...pluginVue.configs["flat/strongly-recommended"],
+  eslintConfigPrettier,
   {
     rules: {
       "vue/no-unused-vars": "error",
