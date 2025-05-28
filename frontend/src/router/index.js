615c736aef4c365c592302d275bf9d04149de75c
Imad Saddik <simad3647@gmail.com>
2025-05-28 09:40:08 +0100

    Used import.meta.env to access the env variables

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,77 +1,77 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '@/views/HomeView.vue'
-import TodayPictureView from '@/views/TodayPictureView.vue'
-import TrendingView from '@/views/TrendingView.vue'
-import LogInView from '@/views/LogInView.vue'
-import SignUpView from '@/views/SignUpView.vue'
-import FavouritesView from '@/views/FavouritesView.vue'
-import ResetPasswordView from '@/views/ResetPasswordView.vue'
-import ResetPasswordConfirmationView from '@/views/ResetPasswordConfirmationView.vue'
-import ActivateAccountView from '@/views/ActivateAccountView.vue'
-import AboutView from '@/views/AboutView.vue'
-import ContributeView from '@/views/ContributeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "@/views/HomeView.vue";
+import TodayPictureView from "@/views/TodayPictureView.vue";
+import TrendingView from "@/views/TrendingView.vue";
+import LogInView from "@/views/LogInView.vue";
+import SignUpView from "@/views/SignUpView.vue";
+import FavouritesView from "@/views/FavouritesView.vue";
+import ResetPasswordView from "@/views/ResetPasswordView.vue";
+import ResetPasswordConfirmationView from "@/views/ResetPasswordConfirmationView.vue";
+import ActivateAccountView from "@/views/ActivateAccountView.vue";
+import AboutView from "@/views/AboutView.vue";
+import ContributeView from "@/views/ContributeView.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: "/",
+    name: "home",
+    component: HomeView,
   },
   {
-    path: '/today',
-    name: 'today',
-    component: TodayPictureView
+    path: "/today",
+    name: "today",
+    component: TodayPictureView,
   },
   {
-    path: '/trending',
-    name: 'trending',
-    component: TrendingView
+    path: "/trending",
+    name: "trending",
+    component: TrendingView,
   },
   {
-    path: '/login',
-    name: 'login',
-    component: LogInView
+    path: "/login",
+    name: "login",
+    component: LogInView,
   },
   {
-    path: '/signup',
-    name: 'signup',
-    component: SignUpView
+    path: "/signup",
+    name: "signup",
+    component: SignUpView,
   },
   {
-    path: '/favourites',
-    name: 'favourites',
-    component: FavouritesView
+    path: "/favourites",
+    name: "favourites",
+    component: FavouritesView,
   },
   {
-    path: '/reset_password',
-    name: 'resetpassword',
-    component: ResetPasswordView
+    path: "/reset_password",
+    name: "resetpassword",
+    component: ResetPasswordView,
   },
   {
-    path: '/activate/:uid/:token',
-    name: 'activate',
-    component: ActivateAccountView
+    path: "/activate/:uid/:token",
+    name: "activate",
+    component: ActivateAccountView,
   },
   {
-    path: '/password/reset/confirm/:uid/:token',
-    name: 'reset_password_confirm',
-    component: ResetPasswordConfirmationView
+    path: "/password/reset/confirm/:uid/:token",
+    name: "reset_password_confirm",
+    component: ResetPasswordConfirmationView,
   },
   {
-    path: '/about',
-    name: 'about',
-    component: AboutView
+    path: "/about",
+    name: "about",
+    component: AboutView,
   },
   {
-    path: '/contribute',
-    name: 'contribute',
-    component: ContributeView
-  }
-]
+    path: "/contribute",
+    name: "contribute",
+    component: ContributeView,
+  },
+];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+});
 
-export default router
+export default router;
