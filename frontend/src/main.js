94951932b426d253611419777231fa781610bc02
Imad Saddik <simad3647@gmail.com>
2025-05-28 09:40:34 +0100

    Imported the GIF instead of using require

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,19 +1,20 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import axios from 'axios'
-import VueLazyload from 'vue3-lazyload'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import axios from "axios";
+import VueLazyload from "vue3-lazyload";
+import loadingGif from "@/assets/loading.gif";
 
-axios.defaults.baseURL = 'http://localhost:8000'
+axios.defaults.baseURL = "http://localhost:8000";
 // axios.defaults.baseURL = 'http://192.168.1.10:8000'
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(store)
-app.use(router, axios)
+app.use(store);
+app.use(router, axios);
 app.use(VueLazyload, {
-  loading: require('@/assets/loading.gif')
-})
+  loading: loadingGif,
+});
 
-app.mount('#app')
+app.mount("#app");
