6934686e0b6edc1ea175d6198ed3f62bd5ba1873
Imad Saddik <simad3647@gmail.com>
2025-06-02 15:34:16 +0100

    Add dotenv configuration and example environment file for Cypress tests

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from "cypress";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:8080",
     viewportWidth: 1280,
     viewportHeight: 1280,
-    setupNodeEvents(on, config) {},
+    setupNodeEvents(on, config) {
+      config.env.TEST_LOGIN_EMAIL = process.env.TEST_LOGIN_EMAIL;
+      config.env.TEST_LOGIN_PASSWORD = process.env.TEST_LOGIN_PASSWORD;
+      return config;
+    },
   },
 });
