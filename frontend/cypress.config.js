c1429512271c22ffef39bb48047b79dcd41caaee
Imad Saddik <simad3647@gmail.com>
2025-06-03 13:01:39 +0100

    Updated the timeout values in the Cypress config

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -8,6 +8,9 @@ export default defineConfig({
     baseUrl: "http://localhost:8080",
     viewportWidth: 1280,
     viewportHeight: 1280,
+    defaultCommandTimeout: 15000,
+    requestTimeout: 15000,
+    responseTimeout: 15000,
     setupNodeEvents(on, config) {
       config.env.TEST_USER_EMAIL = process.env.TEST_USER_EMAIL;
       config.env.TEST_USER_PASSWORD = process.env.TEST_USER_PASSWORD;
