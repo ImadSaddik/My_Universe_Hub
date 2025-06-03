55ec7d93e5102f8c7cd0143d67eae4b16345f938
Imad Saddik <simad3647@gmail.com>
2025-06-03 16:33:13 +0100

    Extracted the logic related to scrapping APOD into a script and added type hinting

---
+++ backend/gallery/apod_scrapper.py
@@ -0,0 +1,112 @@
+import os
+from datetime import datetime
+
+import google.generativeai as genai
+import requests
+from bs4 import BeautifulSoup
+from bs4.element import Tag
+from dotenv import load_dotenv
+
+from .models import Gallery
+
+
+def add_non_existing_images() -> None:
+    a_tags = get_a_tags()
+
+    for tag in a_tags:
+        item = scrape_a_tag(tag)
+        if item["image_url"] is None:
+            continue
+
+        item["date"] = convert_date(item["date"])
+        try:
+            Gallery.objects.get(date=item["date"])
+            print(f"{item['date']} is already in the database.")
+            break
+        except Gallery.DoesNotExist:
+            print(f"{item['date']} is not in the database.")
+            entry = Gallery.objects.create(
+                date=item["date"],
+                title=item["title"],
+                explanation=item["explanation"],
+                image_url=item["image_url"],
+                authors=item["authors"],
+            )
+
+            entry.save()
+
+
+def convert_date(date: str) -> datetime.date:
+    date = date.replace(":", "")
+    return datetime.strptime(date, "%Y %B %d").date()
+
+
+def get_a_tags() -> list[Tag]:
+    source = requests.get("https://apod.nasa.gov/apod/archivepix.html").text
+    soup = BeautifulSoup(source, "lxml")
+
+    b_tag = soup.find_all("b")[1]
+    return b_tag.find_all("a")
+
+
+def scrape_a_tag(a_tag: Tag) -> dict:
+    dictionary = {}
+
+    date = a_tag.find_previous(string=True).strip()
+    title = a_tag.text.strip()
+    url = f"https://apod.nasa.gov/apod/{a_tag['href']}"
+    image_url, explanation = get_image_and_explanation(url)
+    authors = get_authors(url)
+
+    dictionary["date"] = date
+    dictionary["title"] = title
+    dictionary["url"] = url
+    dictionary["image_url"] = image_url
+    dictionary["explanation"] = explanation
+    dictionary["authors"] = authors
+
+    return dictionary
+
+
+def get_image_and_explanation(url: str) -> tuple[str, str]:
+    source = requests.get(url).text
+    soup = BeautifulSoup(source, "lxml")
+
+    p_tags = soup.find_all("p")
+    img_tag = soup.find("img")
+    explanation = p_tags[2].get_text()
+
+    try:
+        image_url = f"https://apod.nasa.gov/apod/{img_tag['src']}"
+    except Exception:
+        image_url = None
+
+    return image_url, explanation
+
+
+def get_authors(url: str) -> str:
+    source = requests.get(url).text
+    soup = BeautifulSoup(source, "lxml")
+
+    center_tags = soup.find_all("center")
+    credit_center_tag = center_tags[1]
+    authors = extract_authors_with_gemini(credit_center_tag)
+
+    return authors
+
+
+def extract_authors_with_gemini(center_tag: Tag) -> str:
+    load_dotenv()
+    genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+    query = f"""Given the following HTML code snippet, your role is to extract the credit information from the center tag.
+The extracted credit information should be returned as a string and separated by a comma to denote multiple authors.
+Dont't include prefix text like "Image Credit" or "Illustration Credit".
+
+The HTML code snippet is as follows:
+{center_tag}
+"""
+
+    model = genai.GenerativeModel("models/gemini-2.0-flash-001")
+    response = model.generate_content(query)
+    return response.text
--- backend/gallery/views.py
+++ backend/gallery/views.py
@@ -1,26 +1,20 @@
 import json
-import os
 from datetime import datetime
 
-import google.generativeai as genai
-import requests
-from bs4 import BeautifulSoup
 from django.db.models import Q
 from django.http import HttpResponseBadRequest, JsonResponse
-from dotenv import load_dotenv
 from rest_framework.decorators import api_view
+from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from .apod_scrapper import add_non_existing_images
 from .models import Gallery, UserAccount
 from .serializers import GallerySerializer
 
-load_dotenv()
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
-
 
 class getArchive(APIView):
-    def get(self, request, start_index, end_index, format=None):
+    def get(self, request: Request, start_index: int, end_index: int) -> Response:
         entries = Gallery.objects.all()
 
         sliced_entries = entries[start_index:end_index]
@@ -29,53 +23,23 @@ class getArchive(APIView):
 
 
 class getArchiveSize(APIView):
-    def get(self, request, format=None):
+    def get(self, request: Request) -> Response:
         entries = Gallery.objects.all()
         response = {"count": len(entries)}
 
         return Response(response)
 
 
-def addNonExistingImages():
-    a_tags = getATags()
-
-    for tag in a_tags:
-        item = scrape_a_tag(tag)
-        if item["image_url"] is None:
-            continue
-
-        item["date"] = convertDate(item["date"])
-        try:
-            Gallery.objects.get(date=item["date"])
-            print(f"{item['date']} is already in the database.")
-            break
-        except Gallery.DoesNotExist:
-            print(f"{item['date']} is not in the database.")
-            entry = Gallery.objects.create(
-                date=item["date"],
-                title=item["title"],
-                explanation=item["explanation"],
-                image_url=item["image_url"],
-                authors=item["authors"],
-            )
-
-            entry.save()
-
-
-def convertDate(date):
-    date = date.replace(":", "")
-    return datetime.strptime(date, "%Y %B %d").date()
-
-
 @api_view(["GET"])
-def getTodayPicture(request):
+def getTodayPicture(request: Request) -> Response | HttpResponseBadRequest:
     todayEntry = Gallery.objects.all()[0]
     today = datetime.now().today().date()
+    print(f"Request type: {type(request)}")
 
     if today != todayEntry.date:
         print("Today's image is not in the database. Adding it now...")
 
-        addNonExistingImages()
+        add_non_existing_images()
         todayEntry = Gallery.objects.all()[0]
 
         if today != todayEntry.date:
@@ -85,76 +49,8 @@ def getTodayPicture(request):
     return Response(serializer.data)
 
 
-def getATags():
-    source = requests.get("https://apod.nasa.gov/apod/archivepix.html").text
-    soup = BeautifulSoup(source, "lxml")
-
-    b_tag = soup.find_all("b")[1]
-    return b_tag.find_all("a")
-
-
-def scrape_a_tag(a_tag):
-    dictionary = {}
-
-    date = a_tag.find_previous(string=True).strip()
-    title = a_tag.text.strip()
-    url = f"https://apod.nasa.gov/apod/{a_tag['href']}"
-    image, explanation = getImageAndExplanation(url)
-    authors = getAuthors(url)
-
-    dictionary["date"] = date
-    dictionary["title"] = title
-    dictionary["url"] = url
-    dictionary["image_url"] = image
-    dictionary["explanation"] = explanation
-    dictionary["authors"] = authors
-
-    return dictionary
-
-
-def getImageAndExplanation(url):
-    source = requests.get(url).text
-    soup = BeautifulSoup(source, "lxml")
-
-    p_tags = soup.find_all("p")
-    img_tag = soup.find("img")
-    explanation = p_tags[2].get_text()
-
-    try:
-        img_url = f"https://apod.nasa.gov/apod/{img_tag['src']}"
-    except Exception:
-        img_url = None
-
-    return img_url, explanation
-
-
-def getAuthors(url):
-    source = requests.get(url).text
-    soup = BeautifulSoup(source, "lxml")
-
-    center_tags = soup.find_all("center")
-    credit_center_tag = center_tags[1]
-    authors = extractAuthorsWithGemini(credit_center_tag)
-
-    return authors
-
-
-def extractAuthorsWithGemini(center_tag):
-    query = f"""Given the following HTML code snippet, your role is to extract the credit information from the center tag.
-The extracted credit information should be returned as a string and separated by a comma to denote multiple authors.
-Dont't include prefix text like "Image Credit" or "Illustration Credit".
-
-The HTML code snippet is as follows:
-{center_tag}
-"""
-
-    model = genai.GenerativeModel("models/gemini-1.5-flash-002")
-    response = model.generate_content(query)
-    return response.text
-
-
 @api_view(["POST"])
-def likeImage(request):
+def likeImage(request: Request) -> JsonResponse | HttpResponseBadRequest:
     data = json.loads(request.body)
     date = data["date"]
     email = data["email"]
@@ -174,7 +70,7 @@ def likeImage(request):
 
 
 @api_view(["POST"])
-def unlikeImage(request):
+def unlikeImage(request: Request) -> JsonResponse | HttpResponseBadRequest:
     data = json.loads(request.body)
     date = data["date"]
     email = data["email"]
@@ -194,7 +90,7 @@ def unlikeImage(request):
 
 
 @api_view(["GET"])
-def search(request, query, start_index, end_index):
+def search(request: Request, query: str, start_index: int, end_index: int) -> Response:
     search_words = query.split(",")
     search_pattern = r"\b(?:" + "|".join(search_words) + r")\b"
 
@@ -206,7 +102,7 @@ def search(request, query, start_index, end_index):
 
 
 @api_view(["GET"])
-def searchSize(request, query):
+def searchSize(request: Request, query: str) -> Response:
     search_words = query.split(",")
     search_pattern = r"\b(?:" + "|".join(search_words) + r")\b"
 
@@ -217,7 +113,7 @@ def searchSize(request, query):
 
 
 @api_view(["GET"])
-def getSortedArchive(request, start_index, end_index):
+def getSortedArchive(request: Request, start_index: int, end_index: int) -> Response:
     entries = Gallery.objects.order_by("-image_likes_count")
     sliced_entries = entries[start_index:end_index]
     serializer = GallerySerializer(sliced_entries, many=True)
@@ -226,7 +122,7 @@ def getSortedArchive(request, start_index, end_index):
 
 
 @api_view(["GET"])
-def getSortedArchiveSize(request):
+def getSortedArchiveSize(request: Request) -> Response:
     entries = Gallery.objects.order_by("-image_likes_count")
     response = {"count": len(entries)}
 
@@ -234,7 +130,7 @@ def getSortedArchiveSize(request):
 
 
 @api_view(["GET"])
-def getFavouritesArchive(request, email, start_index, end_index):
+def getFavouritesArchive(request: Request, email: str, start_index: int, end_index: int) -> Response:
     user = UserAccount.objects.get(email=email)
     entries = Gallery.objects.filter(liked_by_users=user)
 
@@ -248,7 +144,7 @@ def getFavouritesArchive(request, email, start_index, end_index):
 
 
 @api_view(["GET"])
-def getFavouritesArchiveSize(request, email):
+def getFavouritesArchiveSize(request: Request, email: str) -> Response:
     user = UserAccount.objects.get(email=email)
     entries = Gallery.objects.filter(liked_by_users=user)
 
@@ -258,7 +154,7 @@ def getFavouritesArchiveSize(request, email):
 
 
 @api_view(["POST"])
-def resetPassword(request):
+def resetPassword(request: Request) -> JsonResponse | HttpResponseBadRequest:
     data = json.loads(request.body)
     email = data["email"]
     newPassword = data["newPassword"]
