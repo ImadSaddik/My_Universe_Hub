03a55f1e7ec4cd0c2b1838a595c990411e5c2b11
Imad Saddik <simad3647@gmail.com>
2025-06-17 23:38:00 +0100

    Removed secure SSL settings from production configuration

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -44,11 +44,6 @@ if not DEBUG:
     CSRF_TRUSTED_ORIGINS = ["https://myuniversehub.com", "https://www.myuniversehub.com", "https://104.248.40.52"]
     CSRF_COOKIE_SECURE = True
     SESSION_COOKIE_SECURE = True
-    SECURE_HSTS_SECONDS = 31536000
-    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-    SECURE_HSTS_PRELOAD = True
-    SECURE_SSL_REDIRECT = True
-    SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
 # Application definition
 
