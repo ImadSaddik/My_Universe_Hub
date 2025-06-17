6f6fb5fbcc89ac6652434ede74bc896c842da040
Imad Saddik <simad3647@gmail.com>
2025-06-17 23:32:25 +0100

    Added SECURE_PROXY_SSL_HEADER to settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -48,6 +48,7 @@ if not DEBUG:
     SECURE_HSTS_INCLUDE_SUBDOMAINS = True
     SECURE_HSTS_PRELOAD = True
     SECURE_SSL_REDIRECT = True
+    SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
 # Application definition
 
